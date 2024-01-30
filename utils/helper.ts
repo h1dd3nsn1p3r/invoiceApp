3791a581c26a2da7d324ab2ebfaefe8b8cd131df
Anuj Subedi <struggleage@gmail.com>
2024-01-30 22:13:06 +0545

    :package: Added: Scheduler.
    - :package: Added: Scheduler.

--- utils/helper.ts
+++ utils/helper.ts
@@ -1,6 +1,7 @@
 import fs from "fs";
 import { fileURLToPath } from "url";
 import path from "path";
+import cron from "node-cron";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -121,4 +122,77 @@ export const normalizeQueryParam = (param: string): string | null => {
 	}
 
 	return data;
+};
+
+/**
+ * Cron scheduler.
+ * Schedule a cron job that runs every 24 hours.
+ *
+ * @since 1.0.0
+ */
+export const scheduler = async () => {
+	cron.schedule("* * * * *", async () => {
+		await deleteInvoices().catch((e) => {
+			console.log(e);
+		});
+	});
+};
+
+/**
+ * Delete files inside the assets dir,
+ * if the file is older than 7 days.
+ *
+ * @returns { Promise<boolean> } true if the files are deleted.
+ * @since 1.0.0
+ */
+export const deleteInvoices = async (): Promise<boolean> => {
+	// Get the assets dir.
+	// "../../assets" -> "src/assets"
+	const assetsDir = path.join(__dirname, "../../assets");
+
+	console.log("Inside delete invoice function, assets dirs: " + assetsDir);
+
+	if (!fs.existsSync(assetsDir)) {
+		return false;
+	}
+
+	// Find files that ends with extension .pdf using async readdir.
+	const files = await fs.promises.readdir(assetsDir);
+
+	console.log(files);
+
+	const pdfFiles = files.filter((file) => file.endsWith(".pdf"));
+
+	if (pdfFiles.length === 0) {
+		return false;
+	}
+
+	// Get the current time.
+	const currentTime = new Date().getTime();
+
+	// Delete the files that are older than 7 days.
+	pdfFiles.forEach(async (file) => {
+		const filePath = path.join(assetsDir, file);
+		const fileStat = await fs.promises.stat(filePath);
+
+		// Get the file creation time.
+		const createdTime = fileStat.birthtimeMs;
+
+		// Get the difference between the current time and the file creation time.
+		const diff = currentTime - createdTime;
+
+		// Convert the difference to days.
+		const diffInDays = diff / (1000 * 3600 * 24);
+
+		// Delete the file if the difference is greater than 7 days.
+		if (diffInDays > 7) {
+			fs.unlink(filePath, (err) => {
+				if (err) {
+					throw new Error("Failed to delete the file!");
+				}
+
+				return true;
+			});
+		}
+	});
 };
